1b1f47d7addd53781c095ee61021c1b737b150bd
Pat_M <88506591+paddymac99208@users.noreply.github.com>
2022-08-31 20:27:19 -0700

    code function to grab user inputs and calculate

--- main.js
+++ main.js
@@ -47,79 +47,38 @@ const numberThree = () => {
   return thirdNumber;
 };
 
+const numberFour = (min, max) => {
+  fourthNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  return fourthNumber;
+};
+
+const numberFive = () => {
+  //birthyear - lets add numbers together  ie. 1+9+7+4
+  let year = document.getElementById("userBirthyear").value;
+  let fifthNumber = 0;
+  while (year) {
+    digit = year % 10;
+    fifthNumber += digit;
+    year = (year - digit) / 10;
+    return fifthNumber;
+  }
+  // for (let i = 0; i < year.length; i++) {
+  //   let total = year[i];
+  //   total += parseInt(year[i], 10);
+  //   console.log(total);
+  // }
+};
+
 function generateLotteryNumber() {
   firstNumber = numberOne();
   secondNumber = numberTwo();
   thirdNumber = numberThree();
+  fourthNumber = numberFour(1, 70);
+  fifthNumber = numberFive();
+  sixthNumber = 70;
   let message = document.getElementById("lotteryNumber");
   message.innerHTML = `Your Lucky Lottery Numbers Are<br>${firstNumber} - ${secondNumber} - ${thirdNumber} - ${fourthNumber} - ${fifthNumber} - ${sixthNumber}`;
 }
 
 const userPickedColor = document.getElementById("userColor");
 userPickedColor.addEventListener("change", numberThree);
-
-/******************************
- * THE COMMENTED CODE BELOW IS UNUSED
- * CODE AND NOTES I MAY STILL NEED
- * ~PAT
- *
- *******************************/
-
-// let firstNumber = "";
-// let secondNumber = "";
-// let thirdNumber = "";
-// let fourthNumber = "";
-// let fifthNumber = "";
-// let sixthNumber = "";
-
-// const firstName = document.getElementById("first-name").value;
-// const testing = "grey";
-// // const lastName = document.getElementById("last-name").value;
-// // const userColor = document.getElementById("user-color").value;
-// // const userNumber = document.getElementById("user-number").value;
-// // const birthyear = document.getElementById("user-birthyear").value;
-// // const userEyes = document.getElementById("user-eyes").value;
-
-// //First letter of name will be the multiplier for the math.Random().
-// //All Lottery numbers must be between 0 - 70
-// //add default reference value for color, eyes
-// //Math.random * birthyear.....use the last index for the multiplier and the second to last as the divider
-
-// function randomNumber(value) {
-//   return Math.floor(Math.random() * value);
-// }
-
-// /*******************************
-//    * GENERATE LOTTERY NUMBER NOTES
-//    *
-//    * pull in firstName value once linked, using text for testing
-//   convert string to uppercase then minus 20 which would bring the max number to 70 for a 'Z'
-//   if we are not validating, then run an if else, and if the charcode comes back over 70, set the variable to be 70.
-
-//    * *****************************/
-
-// const testChange = document.getElementById("userColor");
-// testChange.addEventListener("change", test);
-
-// document.getElementById("userColor").onchange = function () {
-//   alert("test message");
-// };
-
-// const test = () => {
-//   alert("test function");
-// };
-
-// function generateLotteryNumber() {
-//   let text = testing.toUpperCase().charCodeAt(0) - 20;
-//   if (text > 70) {
-//     return (firstNumber = 70);
-//   } else {
-//     return (firstNumber = text);
-//   }
-//   console.log(firstNumber);
-// }
-
-// console.log(randomNumber(7));
-
-// console.log(generateLotteryNumber());
-// console.log(firstNumber);
