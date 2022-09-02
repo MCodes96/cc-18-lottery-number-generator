ee08d5f147f39b9fed2a3c2104d9523a261cdec9
Pat_M <88506591+paddymac99208@users.noreply.github.com>
2022-09-02 14:35:36 -0700

    fix lottery number returning undefined

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-let firstNumber = '';
-let secondNumber = '';
-let thirdNumber = '';
-let fourthNumber = '';
-let fifthNumber = '';
-let sixthNumber = '';
+let firstNumber = "";
+let secondNumber = "";
+let thirdNumber = "";
+let fourthNumber = "";
+let fifthNumber = "";
+let sixthNumber = "";
 
 function randomNumber(value) {
   return Math.floor(Math.random() * value);
 }
 
 const numberOne = () => {
-  let x = document.getElementById('firstName').value;
+  let x = document.getElementById("firstName").value;
   let firstNumber = x.charCodeAt(0);
   return firstNumber;
 };
 
 const numberTwo = () => {
   //set min and max for index position based on string length
-  let x = document.getElementById('lastName').value;
+  let x = document.getElementById("lastName").value;
   let index = Math.floor(Math.random() * x.length + 1);
   let secondNumber = x.charCodeAt(index);
-  return secondNumber;
+  return Number(secondNumber);
 };
 
 const numberThree = () => {
   let multiplier = 0;
-  let color = document.getElementById('userColor').value;
+  let color = document.getElementById("userColor").value;
   switch (color) {
-    case 'Red':
+    case "Red":
       multiplier = 45;
       break;
-    case 'Blue':
+    case "Blue":
       multiplier = 128;
       break;
-    case 'Green':
+    case "Green":
       multiplier = 239;
       break;
   }
@@ -41,15 +41,22 @@ const numberThree = () => {
   return thirdNumber;
 };
 
-const numberFour = (min, max) => {
-  let userNumber = document.getElementById('userNumber').value;
-  fourthNumber = Math.floor(Math.random() * (max - min + 1) + min) * userNumber;
+const numberFour = () => {
+  let userNumber = document.getElementById("userNumber").value;
+  fourthNumber = randomNumber(userNumber);
   return fourthNumber;
 };
 
+// const numberFour = (min, max) => {
+//   let userNumber = document.getElementById("userNumber").value;
+//   fourthNumber = Math.floor(Math.random() * (max - min + 1) + min) * userNumber;
+//   //console.log(typeof fourthNumber);
+//   return Number(fourthNumber);
+// };
+
 const numberFive = () => {
   //birthyear - lets add numbers together  ie. 1+9+7+4
-  let year = document.getElementById('userBirthyear').value;
+  let year = document.getElementById("userBirthyear").value;
   let fifthNumber = 0;
   while (year) {
     digit = year % 10;
@@ -66,14 +73,20 @@ const numberFive = () => {
 
 function generateLotteryNumber() {
   firstNumber = numberOne();
+  console.log(firstNumber);
   secondNumber = numberTwo();
+  console.log(secondNumber);
   thirdNumber = numberThree();
-  fourthNumber = numberFour(1, 70);
+  console.log(thirdNumber);
+  fourthNumber = numberFour();
+  console.log(fourthNumber);
   fifthNumber = numberFive();
+  console.log(fifthNumber);
   sixthNumber = 70;
-  let message = document.getElementById('lotteryNumber');
+  console.log(sixthNumber);
+  let message = document.getElementById("lotteryNumber");
   message.innerHTML = `Your Lucky Lottery Numbers Are<br>${firstNumber} - ${secondNumber} - ${thirdNumber} - ${fourthNumber} - ${fifthNumber} - ${sixthNumber}`;
 }
 
-const userPickedColor = document.getElementById('userColor');
-userPickedColor.addEventListener('change', numberThree);
+const userPickedColor = document.getElementById("userColor");
+userPickedColor.addEventListener("change", numberThree);
